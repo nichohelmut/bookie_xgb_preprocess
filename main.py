e518999a72b1f0e39f6bace4a48715d1603150a5
nicholasutikal <nicholasutikal@gmail.com>
2021-09-09 11:13:46 +0200

    adds print statements

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from processor.xgb_processor import PreProcess
 
 
 def main():
+    print("Start with bookie_xgb_preprocess ms")
     pp = PreProcess()
     pp.data_for_predict()
-    return print("Done")
+    return print("Done with bookie_xgb_preprocess ms")
 
 
 if __name__ == "__main__":
